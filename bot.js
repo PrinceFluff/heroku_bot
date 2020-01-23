eda2ab70c606af02872b7c574c246223b6d3f774
PrinceFluff <60169879+PrinceFluff@users.noreply.github.com>
2020-01-23 13:52:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
+// npm install get-file
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 const get = require('get-file');
 
 var Data = {};
 
-
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  function parseREADME() {
+  // initialize
+  console.log('RorgBot Initialized');
+
+  // FETCHREADME FUNCTION
   let rawData = "";
   // fetch data from github
   get('prodzpod/-RorgMod', 'README.md', function(err, res) {
@@ -17,7 +19,7 @@ client.on('ready', () => {
       rawData += d.toString()
     });
     res.on('close', function() { 
-      res.end();
+
     });
   });
 
@@ -43,35 +45,34 @@ client.on('ready', () => {
     Data[match[1]] = '**' + match[1] + '**\n`' + match[2] + '` `' + match[3] + '`' + (match[4] || match[4].length ? ' ' + match[4] : '') + '\n' + match[5]
     match = myRegexp.exec(rawData);
   }
-}
+})
 
-
-});
-
-client.on('message', msg => {function message(args) {
-  let List = []; let exactMatch;
-  let msg = args.replace(/\W/g, '').toLowerCase() // removes non-english letters so - and spaces are ignored, allows follow-up to be searched as follow up or followup
-  for (let key in Data) {
-    if (key.replace(/\W/g, '').toLowerCase() == msg) {
-      exactMatch = key;
-      break;
+client.on("message", message => {
+  if (message.content.startsWith('#')) { // detect prefix
+    let args = message.content.split(/\s+/g).slice(1).join(" "); // remove the command part, leaving the arguments
+    // MESSAGE FUNCTION //
+    let List = []; let exactMatch;
+    let msg = args.replace(/\W/g, '').toLowerCase() // removes non-english letters so - and spaces are ignored, allows follow-up to be searched as follow up or followup
+    for (let key in Data) {
+      if (key.replace(/\W/g, '').toLowerCase() == msg) {
+        exactMatch = key;
+        break;
+      }
+      if (key.replace(/\W/g, '').toLowerCase().indexOf(msg) + 1) // indexOf returns -1 if search fails, -1 + 1 is 0 which is false
+        List.push(key)
     }
-    if (key.replace(/\W/g, '').toLowerCase().indexOf(msg) + 1) // indexOf returns -1 if search fails, -1 + 1 is 0 which is false
-      List.push(key)
-  }
-
-  // lets get this brod
-  if (exactMatch) message.channel.send(Data[exactMatch]); // exact matches are considered first
-  else if (List.length == 0) // Nothing found
-    message.channel.send("I couldn't find a card/relic named __" + args + "__. Maybe it's in the base game?")
-  else if (List.length == 1) // Only 1 thing found
-    message.channel.send(Data[List[0]]) // send that
-  else if (List.length <= 5) // 5 is arbiturary
-    message.channel.send("We found multiple card/relic related to that name. Try: \n**" + List.join("**\n **") + "**")
-  else 
-    message.channel.send("We found too many card/relics with that search term. Try putting in more letters.")
-}
   
+    // lets get this brod
+    if (exactMatch) message.channel.send(Data[exactMatch]); // exact matches are considered first
+    else if (List.length == 0) // Nothing found
+      message.channel.send("I couldn't find a card/relic named __" + args + "__. Maybe it's in the base game?")
+    else if (List.length == 1) // Only 1 thing found
+      message.channel.send(Data[List[0]]) // send that
+    else if (List.length <= 5) // 5 is arbiturary
+      message.channel.send("We found multiple card/relic related to that name. Try: \n**" + List.join("**\n **") + "**")
+    else 
+      message.channel.send("We found too many card/relics with that search term. Try putting in more letters.")
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
